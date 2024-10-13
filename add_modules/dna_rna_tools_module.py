865c77c9038a7de0e6dc9731d28cb363e1a689fd
“Yulia” <cloudatlasfrrs@gmail.com>
2024-10-13 03:40:58 +0300

    Fix logic usage, move iteration to bioinf_tools

--- add_modules/dna_rna_tools_module.py
+++ add_modules/dna_rna_tools_module.py
@@ -2,50 +2,41 @@ nucl_dna = {"a", "t", "g", "c", "A", "T", "G", "C"}
 nucl_rna = {"a", "u", "g", "c", "A", "U", "G", "C"}
     
 def is_na(seq: str) -> bool:
-    if set(seq) <= (nucl_dna) or set(seq) <= (nucl_rna):
-        return True
-    else:
-        return False
+	    return set(seq) <= (nucl_dna) or set(seq) <= (nucl_rna)
+
 
 transcribe_dict = {"T": "U", "t": "u"}
 
-def transcribe(*n_seqs: list) -> list:
-    transcribed_seqs = []
-    for seq in n_seqs:
-        res1 = []
-        for char in [*seq]:
-            res1.append(transcribe_dict.get(char, char))
-            res2 = "".join(res1)
-        transcribed_seqs.append(res2)
-    return transcribed_seqs
+
+def transcribe(seq: str) -> str:
+    res1 = []
+    for char in seq:
+        res1.append(transcribe_dict[char] if char in transcribe_dict else char)
+        res2 = "".join(res1)
+    return res2
 
 
-def reverse(*n_seqs: list) -> list:
-    reversed_seqs = []
-    for seq in n_seqs:
-        reversed_seq = seq[::-1]
-        reversed_seqs.append(reversed_seq)
-    return reversed_seqs
+def reverse(seq: str) -> str:
+    return seq[::-1]
+
 
 complement_dict = {
-        "A": "T",
-        "G": "C",
-        "a": "t",
-        "g": "c",
-        "T": "A",
-        "C": "G",
-        "t": "a",
-        "c": "g",
-        "U": "T",
-        "u": "t",
-        }
+    "A": "T",
+    "G": "C",
+    "a": "t",
+    "g": "c",
+    "T": "A",
+    "C": "G",
+    "t": "a",
+    "c": "g",
+    "U": "T",
+    "u": "t",
+}
 
-def complement(*n_seqs: list) -> list:
-    complemented_seqs = []
-    for seq in n_seqs:
-        res1 = []
-        for char in [*seq]:
-            res1.append(complement_dict.get(char, char))
-            res2 = "".join(res1)
-        complemented_seqs.append(res2)
-    return complemented_seqs
+
+def complement(seq: str) -> str:
+    res1 = []
+    for char in seq:
+        res1.append(complement_dict[char] if char in complement_dict else char)
+        res2 = "".join(res1)
+    return res2
