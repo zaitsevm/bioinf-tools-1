fd56470549ea4e9df69a13b7e101f1b34915a478
“Yulia” <cloudatlasfrrs@gmail.com>
2024-10-13 03:44:33 +0300

    Fix naming

--- add_modules/filter_fastq_module.py
+++ add_modules/filter_fastq_module.py
@@ -1,12 +1,12 @@
 
-def length_control(seq_length: int, bounds: tuple or num) -> bool: 
+def check_length(seq_length: int, bounds: tuple or num) -> bool: 
     if type(bounds) == tuple:
     	return bounds[0] <= seq_length <= bounds[1]
     else:
     	return seq_length <= bounds
 
 
-def gc_control(sequence: str, bounds: tuple or num) -> bool:
+def check_gc(sequence: str, bounds: tuple or num) -> bool:
     gc_content = 100 * (sequence.count("C") + sequence.count("G")) / len(sequence)
     if type(bounds) == tuple:
     	return bounds[0] <= gc_content <= bounds[1]
